c55006e2a5325ef5bce8305912e290855dce8342
ChaoJie Yang <cj.yang0225@gmail.com>
2022-06-15 00:12:37 +0800

    feat: add static data for navbar

---
+++ src/data/index.js
@@ -0,0 +1,32 @@
+const NEWS_CATEGORIES = [
+  {
+    category: 'top',
+    text: '焦點',
+  },
+  {
+    category: 'business',
+    text: '商業',
+  },
+  {
+    category: 'entertainment',
+    text: '娛樂',
+  },
+  {
+    category: 'health',
+    text: '健康',
+  },
+  {
+    category: 'science',
+    text: '科學',
+  },
+  {
+    category: 'techology',
+    text: '科技',
+  },
+  {
+    category: 'sports',
+    text: '體育',
+  },
+];
+
+export { NEWS_CATEGORIES };

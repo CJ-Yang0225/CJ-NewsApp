6b9510007321275a09594f350c6af8ace27f25e0
ChaoJie Yang <cj.yang0225@gmail.com>
2022-06-15 18:04:30 +0800

    feat: add Header to collections page

--- src/pages/collections.js
+++ src/pages/collections.js
@@ -1,1 +1,22 @@
-document.getElementById('app').innerHTML = `已收藏的新聞`;
+import Header from '../components/Header';
+
+(function (doc) {
+  const oApp = doc.getElementById('app');
+
+  const init = () => {
+    render();
+  };
+
+  function render() {
+    const headerTpl = Header.setState({
+      title: '已收藏的新聞',
+      backUrl: './index.html',
+      showBackIcon: true,
+      showCollectionIcon: false,
+    });
+
+    oApp.innerHTML += headerTpl;
+  }
+
+  init();
+})(document);

3ae633b8e1420786c62c0c561b4bfa1160ddf5a7
ChaoJie Yang <cj.yang0225@gmail.com>
2022-06-16 22:47:29 +0800

    feat: add Navbar to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,39 @@
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+
+const store = {
+  category: 'top',
+};
 
 (function (doc) {
   const oApp = doc.getElementById('app');
 
   const init = () => {
     render();
+    useEvent();
   };
 
   init();
 
   function render() {
+    const { category } = store;
+
     const headerTpl = Header.setProps({
       title: '頭條新聞',
       backUrl: 'javascript:;',
       showBackIcon: false,
       showCollectionIcon: true,
     });
+    const navbarTpl = Navbar.setProps({ activatedCategory: category });
 
-    oApp.innerHTML += headerTpl;
+    oApp.innerHTML += headerTpl + navbarTpl;
+  }
+
+  function useEvent() {
+    Navbar.onSwitch(switchCategory);
+  }
+
+  function switchCategory(category) {
+    store.category = category;
   }
 })(document);

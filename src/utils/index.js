9348ba5668515d2951d92c1e23b62abde096ab35
ChaoJie Yang <cj.yang0225@gmail.com>
2022-06-19 15:31:35 +0800

    feat: add utility function to format url params

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,18 @@
-function injectTpl(template, data) {
+const injectTpl = (template, data) => {
   return template.replace(/\{\{(.*?)\}\}/g, (_, key) => {
     return data[key.trim()];
   });
-}
+};
 
-export { injectTpl };
+const formatParams = (url = '/api', params, formatter) => {
+  /\?$/.test(url) || (url += '?');
+
+  url += params
+    .map((param) => formatter(param).join('='))
+    .filter(Boolean)
+    .join('&');
+
+  return url;
+};
+
+export { injectTpl, formatParams };

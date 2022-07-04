94b427bae62b44e66330b5c6fe71ba0d0977b467
ChaoJie Yang <cj.yang0225@gmail.com>
2022-07-04 15:44:55 +0800

    feat(components): add PullHint

---
+++ src/components/PullHint/PullHint.tpl
@@ -0,0 +1,6 @@
+<div class="pull-hint {{ status }}">
+  {{ Icon }}
+  <p class="pull-hint__text">
+    {{ text }}
+  </p>
+</div>
---
+++ src/components/PullHint/index.js
@@ -0,0 +1,28 @@
+import './PullHint.scss';
+import PullHintTpl from './PullHint.tpl';
+import { injectTpl } from '../../utils';
+import Icon from '../Icon';
+import iconUrl from '../../assets/images/spin_loading.gif';
+
+export default {
+  name: 'PullHint',
+  create(props) {
+    const { status, text } = props;
+
+    return injectTpl(PullHintTpl, {
+      status: status ? ` pull-hint--${status}` : '',
+      Icon: Icon.create({ iconUrl }),
+      text:
+        text ||
+        (status === 'loading'
+          ? '載入更多新聞中'
+          : status === 'no-data'
+          ? '沒有更多新聞了'
+          : ''),
+    });
+  },
+  removeHintFrom(parentEl) {
+    const oPullHint = parentEl?.querySelector('.pull-hint');
+    oPullHint?.remove();
+  },
+};

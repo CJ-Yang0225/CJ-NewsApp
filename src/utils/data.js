e76835c4708e08f80ef0fdc6eaa04c9177c1b01d
ChaoJie Yang <cj.yang0225@gmail.com>
2022-07-05 01:21:47 +0800

    refactor(utils): rename and move files

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,3 +1,15 @@
+/**
+ * @typedef {string} Tpl
+ * @param {Tpl} Template
+ * @param {Object.<string, string>} data
+ * @returns {Tpl} injectedTemplate
+ */
+const injectTpl = (Template, data) => {
+  return Template.replace(/\s*\{\{(.*?)\}\}\s*/g, (_, key) => {
+    return data[key.trim()];
+  });
+};
+
 const formatParams = (url = '/api', params, formatter) => {
   /\?$/.test(url) || (url += '?');
 
@@ -23,4 +35,4 @@ const sliceNewsByCount = (news = [], count = 10) => {
   return slicedNews;
 };
 
-export { formatParams, sliceNewsByCount };
+export { injectTpl, formatParams, sliceNewsByCount };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export * from './template';
 export * from './data';
-export * from './uiux';
+export * from './dom';
+export * from './view';
 export { default as EventStore } from './event';

5e8fe1b38a5300e438930aa286ed00626723051c
ChaoJie Yang <cj.yang0225@gmail.com>
2022-07-11 17:14:39 +0800

    feat(pages): add collections page for displaying and managing news cards

--- src/pages/collections.js
+++ src/pages/collections.js
@@ -1,11 +1,25 @@
+import '../styles/collections.scss';
 import Header from '../components/Header';
+import { getDataFromLocalStorage, setDataToLocalStorage } from '../utils';
+import NewsCard from '../components/NewsCard';
+import { BOOKMARKS_ITEM } from '../constants/news';
+import { REDIRECT_TO_DETAIL, TOGGLE_BOOKMARK } from '../constants/actionTypes';
 
-(function (doc) {
+(function (doc, getBookmarks) {
+  const state = {
+    editMode: false,
+    bookmarkForEditing: [],
+  };
+
   const oApp = doc.getElementById('app');
 
   const init = () => {
     render();
+    populateNews();
+    useEvent();
   };
+
+  init();
 
   function render() {
     const headerTpl = Header.create({
@@ -13,10 +27,77 @@ import Header from '../components/Header';
       backUrl: './index.html',
       showBackIcon: true,
       showCollectionIcon: false,
+      tpl: '<button id="management">管理</button>',
     });
 
     oApp.innerHTML += headerTpl;
+    oApp.appendChild(NewsCard.Container('all'));
   }
 
-  init();
-})(document);
+  function useEvent() {
+    NewsCard.onClick(dispatchAction);
+    oApp.querySelector('#management').addEventListener('click', switchEidtMode);
+  }
+
+  function switchEidtMode(event) {
+    const oButton = event.currentTarget;
+    const oNewsContainer = oApp.querySelector('.news-container');
+
+    const prevEditMode = state.editMode;
+    state.editMode = !state.editMode;
+
+    if (state.editMode) {
+      state.bookmarkForEditing = getBookmarks();
+      console.log(state.bookmarkForEditing);
+
+      oButton.textContent = '確定';
+      oNewsContainer.classList.add('news-container--editing');
+    } else {
+      oButton.textContent = '管理';
+      oNewsContainer.classList.remove('news-container--editing');
+    }
+
+    if (prevEditMode === true && state.editMode === false) {
+      setDataToLocalStorage(BOOKMARKS_ITEM, state.bookmarkForEditing);
+      oNewsContainer.innerHTML = '';
+      populateNews();
+    }
+  }
+
+  function populateNews() {
+    const oNewsContainer = oApp.querySelector('.news-container');
+
+    const newsCardList = NewsCard.createList(getBookmarks());
+    oNewsContainer.appendChild(newsCardList);
+
+    NewsCard.triggerImagesFadeIn();
+  }
+
+  function dispatchAction(action) {
+    const { type, payload } = action;
+
+    switch (type) {
+      case REDIRECT_TO_DETAIL:
+        redirectToDetail(payload);
+        break;
+
+      case TOGGLE_BOOKMARK:
+        toggleBookmark(payload);
+        break;
+    }
+  }
+
+  function redirectToDetail({ index }) {
+    const bookmarks = getBookmarks();
+    const currentNews = bookmarks[index];
+    location.href = currentNews.url;
+  }
+
+  function toggleBookmark({ index, isMarked }) {
+    if (state.editMode && !isMarked) {
+      state.bookmarkForEditing = state.bookmarkForEditing.filter(
+        (_, bookmarkIndex) => bookmarkIndex != index
+      );
+    }
+  }
+})(document, getDataFromLocalStorage.bind(window, BOOKMARKS_ITEM));

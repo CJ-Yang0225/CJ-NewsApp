785a8a5dd553c44915c0f465830939dd6cf19a8f
ChaoJie Yang <cj.yang0225@gmail.com>
2022-07-21 23:58:15 +0800

    chore(services): add comment

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ const NODE_ENV = process.env.NODE_ENV;
 
 const config = {
   development: {
-    PROXY_SERVER_HOST: 'http://localhost:3000',
+    PROXY_SERVER_HOST: location.origin, //'http://localhost:3000'
   },
   production: {
     PROXY_SERVER_HOST: 'https://cj-news-server.vercel.app',

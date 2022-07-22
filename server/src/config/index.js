4eeef59260c894fe1bf80673264ea9c93683d901
ChaoJie Yang <cj.yang0225@gmail.com>
2022-07-23 02:46:04 +0800

    feat(server): update interceptors of axios instance

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -4,24 +4,42 @@ require('dotenv').config({ path: resolve(process.cwd(), '.env.local') });
 
 const NEWS_API_URL = 'https://newsapi.org/v2/top-headlines?country=tw';
 
-const instance = axios.create({
+const newsApiKeyNames = Object.keys(process.env).filter((prop) =>
+  prop.startsWith('NEWS_API_KEY')
+);
+
+let number = 0;
+let currentApiKeyName = newsApiKeyNames[number];
+
+const axiosInstance = axios.create({
   baseURL: NEWS_API_URL,
-  headers: { 'X-Api-Key': process.env.NEWS_API_KEY },
+  headers: { 'X-Api-Key': process.env[currentApiKeyName] },
 });
 
 async function request(method, params, config) {
-  const result = await instance.request({ method, params, ...config });
+  console.log("Current API Key's Name:", currentApiKeyName);
+
+  const result = await axiosInstance.request({ method, params, ...config });
   const data = result.data;
 
   return data;
 }
 
-instance.interceptors.response.use(
+axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    console.error('News API Error:', error);
+    console.error('News API Error:', error.response.data);
+
     if (error.response.status === 429) {
-      instance.defaults.headers['X-Api-Key'] = process.env.NEWS_API_KEY2;
+      if (number < newsApiKeyNames.length - 1) {
+        number++;
+      } else {
+        number = 0;
+      }
+
+      currentApiKeyName = newsApiKeyNames[number];
+      axiosInstance.defaults.headers['X-Api-Key'] =
+        process.env[currentApiKeyName];
     }
 
     return Promise.reject(error);

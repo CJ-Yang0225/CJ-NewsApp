25a22061013c05cc58e56b5f8f5f1a5b82b28afd
ChaoJie Yang <cj.yang0225@gmail.com>
2022-07-23 01:41:12 +0800

    chore(constants): change the order of categories

--- src/constants/news.js
+++ src/constants/news.js
@@ -4,28 +4,28 @@ const NEWS_LABELS = [
     text: '焦點',
   },
   {
-    category: 'business',
-    text: '商業',
-  },
-  {
     category: 'entertainment',
     text: '娛樂',
+  },
+  {
+    category: 'sports',
+    text: '體育',
+  },
+  {
+    category: 'business',
+    text: '商業',
   },
   {
     category: 'health',
     text: '健康',
   },
   {
-    category: 'science',
-    text: '科學',
-  },
-  {
     category: 'technology',
     text: '科技',
   },
   {
-    category: 'sports',
-    text: '體育',
+    category: 'science',
+    text: '科學',
   },
 ];
 
